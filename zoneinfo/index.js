4e11f53631ba996427ff4f8997d892aa69ea2a88
Adam Ahmed <aahmed@atlassian.com>
2012-04-29 19:21:34 +1000

    More sensible endpoints. Demo works.

--- zoneinfo/index.js
+++ zoneinfo/index.js
@@ -19,14 +19,12 @@ function handleNonJSON(req, res, next) {
 	}
 }
 
-function handleGetZonesForCities(req, res, next) {
-	var cities = req.param('city') || [];
-	if (typeof cities === 'string') cities = [ cities ];
+function handleGetTzInfo(req, res, next) {
+	var zones = req.param('zone') || [];
+	if (typeof zones === 'string') zones = [ zones ];
 
-	console.log("cities:" + cities);
-
-	if (cities.length) {
-		timezoneJSparser.processFileset(cities, conf.dataDir, conf.dataEncoding, function(err, data) {
+	if (zones.length) {
+		timezoneJSparser.processFileset(zones, conf.dataDir, conf.dataEncoding, function(err, data) {
 			if (err) next(err);
 			else res.send(data);
 		});
@@ -36,7 +34,7 @@ function handleGetZonesForCities(req, res, next) {
 
 
 var zones;
-function handleListCities(req, res, next) {
+function handleListZones(req, res, next) {
 
 	timezoneJSparser.ensureFilesLoadedSync(conf.dataDir, conf.dataEncoding);
 
@@ -47,7 +45,7 @@ function handleListCities(req, res, next) {
 		}
 	}
 
-	res.send(zones);
+	res.send({ zones : zones });
 
 }
 
@@ -60,66 +58,92 @@ exports.createApp = function() {
 	app.get('/', function(req, res) {
 		res.contentType("text/html");
 		res.send('<!DOCTYPE html>\n<title>Zoneinfo Server</title>' +
-			'<p>This service has two JSON (via CORS) and JSONP endpoints. Add a ?callback=fn_name param for a JSONP response.</p>' +
+			'<p>This site serves <a href="http://en.wikipedia.org/wiki/Tz_database">Olson</a> timezone information from <a href="http://www.iana.org/time-zones">IANA</a></p>' +
+			'<p>It has two JSON (via CORS) and JSONP endpoints. Add a ?callback=fn_name param to get a JSONP response.</p>' +
 			'<ul>' +
-				'<li>Use <a href="./cities">/cities</a> to see the list cities you can obtain timezone info for.</li>' +
-				'<li>Use <a href="./zones?city=America/Chicago&city=America/New_York&callback=fn_name">/zones?city=X&city=Y&callback=fn_name</a> to get timezone info you can use with <a href="https://github.com/mde/timezone-js">timezone-js</a> (or however you like).</li>' +
+				'<li>Use <a href="./zones">/zones</a> to see the list of zones you can obtain timezone info for.</li>' +
+				'<li>Use <a href="./tzinfo?zone=America/Chicago&zone=America/New_York&callback=fn_name">/tzinfo[?zone=X[&zone=Y[&callback=fn_name]]]</a> to get timezone info you can use with <a href="https://github.com/mde/timezone-js">timezone-js</a> (or however you like).</li>' +
 			'</ul>' +
-			'<p>Source on <a href="">GitHub</a> and <a href="">Bitbucket</a>.</p>' +
+			'<p><a href="./demo">Demo</a></p>' +
+			'<p>Source on <a href="https://github.com/hitsthings/zoneinfo-server">GitHub</a>.</p>' +
 			'<p>Made with JS, love, and <a href="http://noiregrets.com">noir egrets</a> by <a href="http://www.twitter.com/hitsthings">@hitsthings</a>.</p>');
 	});
 
-	app.get('/zones', allowCORS, handleNonJSON, handleGetZonesForCities, function(request, response) {
-	  response.send('To use this service, specify cities using query string parameters. E.g., /zones?city=America/Chicago&city=America/New_York');
+	app.get('/tzinfo', allowCORS, handleNonJSON, handleGetTzInfo, function(request, response) {
+	  response.send('To use this service, specify locations using query string parameters. E.g., /tzinfo?zone=America/Chicago&zone=America/New_York');
 	});
 
-	app.get('/cities', allowCORS, handleNonJSON, handleListCities);
+	app.get('/zones', allowCORS, handleNonJSON, handleListZones);
 
-
-function demo() {
-	// timezone-js needs to remove it's dependency on Fleegix...
-	fleegix.xhr = {
-		doGet : function(success, url) { $.ajax(url, { processData: false }).done(function(data) { success(JSON.stringify(data)); }); }
-	};
-
-
-	var _tz = timezoneJS.timezone;
-	_tz.loadingScheme = _tz.loadingSchemes.MANUAL_LOAD;
-	_tz.zoneFileBasePath = 'stop hassling me';
-	_tz.loadZoneJSONData('./zones?city=America/Los_Angeles&city=America/New_York&city=Australia/Sydney&city=Asia/Tokyo');
-	
-	var div = document.createElement('div');
-	document.body.appendChild(div);
-
-	function getTimes() {
-		var now = new Date(),
-		nowLA = new timezoneJS.Date(now)
-		'America/Los_Angeles'),
-		nowNY = new timezoneJS.Date(now)
-		'America/New_York'),
-		nowSyd = new timezoneJS.Date(now)
-		 'Australia/Sydney'),
-		nowTokyo = new timezoneJS.Date(now)
-		'Asia/Tokyo');
-
-	
-		div.innerHTML = '<dl>' +
-				'<dt>You</dt><dd>' + now.toString() + '</dd>' +
-				'<dt>Los Angeles</dt><dd>' + nowLA.toString() + '</dd>' +
-				'<dt>New York</dt><dd>' + nowNY.toString() + '</dd>' +
-				'<dt>Sydney</dt><dd>' + nowSyd.toString() + '</dd>' +
-				'<dt>Tokyo</dt><dd>' + nowTokyo.toString() + '</dd>' +
-			'</dl>';
-	}
-	
-	setInterval(getTimes, 1000);		
-}
 	app.get('/demo', function(req, res) {
-		res.send('<!DOCTYPE html>\n<head><title>Demo page</title><script src="http://code.jquery.com/jquery-1.7.2.min.js"></script><script src="https://raw.github.com/mde/timezone-js/master/src/date.js"></script><style>script { display: block; white-space: pre; }</style></head>' +
+		res.send('<!DOCTYPE html>\n' +
+			'<head>' +
+				'<title>Demo page</title>' +
+				'<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>' +
+				'<script src="https://raw.github.com/mde/timezone-js/51db759d249a23662bf2c3c7cc7fed4a8662580a/src/date.js"></script>' +
+				'<style>script { display: block; white-space: pre; }</style>' +
+			'</head>' +
 			'<body>' +
-				'<script>\n' + demo.toString() + '\ndemo();</script>' +
+				'<div id="clock">Loading clock...</div>' +
+				'<script>' + demo.toString() + '\ndemo();</script>' +
 			'</body>');
 	});
 
 	return app;
 };
+
+
+//client-side JS
+function demo() {
+
+	function setupTimezoneJS() {
+		// timezone-js needs to remove it's dependency on Fleegix...
+		fleegix.xhr = {
+			doGet : function(success, url) { $.ajax(url, { processData: false, async: false }).done(function(data) { success(JSON.stringify(data)); }); }
+		};
+
+		var _tz = timezoneJS.timezone;
+		_tz.loadingScheme = _tz.loadingSchemes.MANUAL_LOAD;
+		_tz.zoneFileBasePath = 'stop hassling me';
+
+		// add a method for converting between timezones. Don't trust it - probably buggy.
+		timezoneJS.Date.prototype.inTimezone = function(tz) {
+			var date = new timezoneJS.Date();
+			var self = this;
+
+			date.setTimezone(tz);
+			function pipe(type) { date['setUTC' + type](self['getUTC' + type]()); }
+
+			['Milliseconds', 'Seconds', 'Minutes', 'Hours', 'Date', 'Month', 'FullYear'].forEach(pipe);
+
+			return date;
+		};
+	}
+	setupTimezoneJS();
+	
+	// USAGE: load timezone information for LA, NYC, Sydney, and Tokyo.
+	timezoneJS.timezone.loadZoneJSONData('./tzinfo?zone=America/Los_Angeles&zone=America/New_York&zone=Australia/Sydney&zone=Asia/Tokyo');
+	
+	function displayIntlClock() {
+		var clock = document.getElementById('clock');
+
+		function getTimes() {
+			var now = new timezoneJS.Date(),
+			nowLA = now.inTimezone('America/Los_Angeles'),
+			nowNY = now.inTimezone('America/New_York'),
+			nowSyd = now.inTimezone('Australia/Sydney'),
+			nowTokyo = now.inTimezone('Asia/Tokyo');
+
+			clock.innerHTML = '<dl>' +
+					'<dt>You</dt><dd>' + now.toString() + '</dd>' +
+					'<dt>Los Angeles</dt><dd>' + nowLA.toString() + '</dd>' +
+					'<dt>New York</dt><dd>' + nowNY.toString() + '</dd>' +
+					'<dt>Sydney</dt><dd>' + nowSyd.toString() + '</dd>' +
+					'<dt>Tokyo</dt><dd>' + nowTokyo.toString() + '</dd>' +
+				'</dl>';
+		}
+		
+		setInterval(getTimes, 1000);
+	}
+	displayIntlClock();
+}
